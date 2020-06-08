af48048cc89aa5f7eb0184f5a87b859c576a8135
Anubha Bangia <anubha.bangia@gmail.com>
2020-06-08 15:12:27 +0530

    Made changes in the code sheet

--- FirstScript.R
+++ FirstScript.R
@@ -10,3 +10,32 @@ x2 <- (-b-sqrt(b^2-4*a*c))/(2*a)
 #Code = log(x, base = exp(1))
 #To calculate log with base 2 of 1024
 log(1024, base = 2)
+
+#Calcuating rows and variables in the data set
+nrow(datasetname)
+str(datasetname)
+
+#Calculating number of levels of a factor.
+nlevels(datasetname$factorname)
+
+#Order,sort,rank functions
+
+#Order function which re-arranges its first argument into ascending or descending order
+#and returns the index of that arranged number.
+x <- c(2,43,27,95,17)
+order(x)
+
+#Sort function: sort a vector or factor in ascending or descending order.
+sort(x) 
+              Or
+sort(x, decreasing = FALSE)
+ 
+#rank function: returns the sample ranks of the values in a vector.
+
+rank(x)
+
+
+
+
+
+
