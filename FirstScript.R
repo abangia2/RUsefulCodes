577d4dd2d559a8937925cda4bc97b2b16bb38a8f
Anubha Bangia <anubha.bangia@gmail.com>
2020-06-08 13:35:23 +0530

    second commit

--- FirstScript.R
+++ FirstScript.R
@@ -5,3 +5,8 @@ b <- -3
 c <- 4
 x1 <- (-b+sqrt(b^2-4*a*c))/(2*a)
 x2 <- (-b-sqrt(b^2-4*a*c))/(2*a)
+
+#Calculating Log with base
+#Code = log(x, base = exp(1))
+#To calculate log with base 2 of 1024
+log(1024, base = 2)
